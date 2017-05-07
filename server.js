714294b8e34e6c833c42c237dd400da0a0274676
Admin <admin@Admins-Mac.local>
2017-05-07 18:24:52 +0530

    error rectified

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express     = require('express'),
 const PORT      = process.env.PORT || 3000;
 
     app.use(function(req, res, next){
-      if(req.header['x-forwarded-proto'] === 'https'){
+      if(req.headers['x-forwarded-proto'] === 'https'){
         res.redirect('http://' + req.hostname + req.url);
       } else {
         next();
