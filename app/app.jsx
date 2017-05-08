6437fcb83a6a5b8c98428aca61280779de2a261a
Admin <admin@Admins-Mac.local>
2017-05-08 18:44:25 +0530

    Added foundation & jquery

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,11 @@ var About         = require('About');
 var Examples         = require('Examples');
 
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+
 ReactDOM.render(
       <Router history={hashHistory}>
         <Route path="/" component={Main}>
